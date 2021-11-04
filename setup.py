2317ec85f8f5457707bb21e6bc8d8821b07f6e38
LaoshuBaby <keaitianxinmail@qq.com>
2021-11-04 23:54:04 +0800

    Remove dist
    This version canbe found in
    
    http://web.archive.org/web/20211104154838/https://pypi.org/project/keqing-sword-LaoshuBaby/

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",
-    version="0.0.1",
-    author="OSMChina",
+    name="keqing_sword-LaoshuBaby",
+    version="0.1.0",
+    author="LaoshuBaby",
     author_email="keaitianxinmail@qq.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/osmchina/osmchina-keqing_sword",
     project_urls={
         "Bug Tracker": "https://github.com/osmchina/osmchina-keqing_sword/issues",
     },
