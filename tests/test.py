d62563d1a3e8aa3326204d38a5a74fbe4f7b3022
LaoshuBaby <keaitianxinmail@qq.com>
2022-07-09 18:19:53 +0800

    fix(test.py): 完善Web和JOSM用例，手工检验过数据

--- tests/test.py
+++ tests/test.py
@@ -4,17 +4,14 @@ from os.path import dirname, join, realpath
 from kqs.waifu import Waifu
 
 
-# A simple example for testing
 class TestClass(unittest.TestCase):
-    data_path = join(
-        dirname(realpath(__file__)), "export", "OSMWebsite_export.osm"
-    )
-
     def setUp(self) -> None:
         self.map = Waifu()
-        self.map.read_file(self.data_path)
 
-    def test_entity_count(self):
+    def test_entity_count_OSMWebsite(self):
+        FILENAME = "OSMWebsite_export.osm"
+        data_path = join(dirname(realpath(__file__)), "export", FILENAME)
+        self.map.read_file(data_path)
         m = self.map
         assert len(m.bounds_list) == 1
         assert len(m.node_dict) == 11818
@@ -22,6 +19,9 @@ class TestClass(unittest.TestCase):
         assert len(m.relation_dict) == 14
 
     def test_entity_count_JOSM(self):
+        FILENAME = "JOSM_export.osm"
+        data_path = join(dirname(realpath(__file__)), "export", FILENAME)
+        self.map.read_file(data_path)
         m = self.map
         assert len(m.bounds_list) == 1
         assert len(m.node_dict) == 539
@@ -29,11 +29,14 @@ class TestClass(unittest.TestCase):
         assert len(m.relation_dict) == 5
 
     def test_entity_count_level0(self):
+        FILENAME = "level0_export.osm"
+        data_path = join(dirname(realpath(__file__)), "export", FILENAME)
+        self.map.read_file(data_path)
         m = self.map
         # level0 test failed because current program can't handle element without lat, e.g.: <node id='3328159064' version='2' action='delete' timestamp='2021-11-28T05:14:24+00:00'>
         assert len(m.bounds_list) == 0
-        assert len(m.node_dict) == 0
-        assert len(m.way_dict) == 0
+        assert len(m.node_dict) == 478
+        assert len(m.way_dict) == 22
         assert len(m.relation_dict) == 0
 
 
