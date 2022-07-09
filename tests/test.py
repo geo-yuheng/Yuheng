73c10e0dd82fa354f6798df986184fda0636ab2b
LaoshuBaby <keaitianxinmail@qq.com>
2022-07-09 18:07:16 +0800

    fix(test.py): More export format test

--- tests/test.py
+++ tests/test.py
@@ -21,6 +21,21 @@ class TestClass(unittest.TestCase):
         assert len(m.way_dict) == 2376
         assert len(m.relation_dict) == 14
 
+    def test_entity_count_JOSM(self):
+        m = self.map
+        assert len(m.bounds_list) == 1
+        assert len(m.node_dict) == 539
+        assert len(m.way_dict) == 67
+        assert len(m.relation_dict) == 5
+
+    def test_entity_count_level0(self):
+        m = self.map
+        # level0 test failed because current program can't handle element without lat, e.g.: <node id='3328159064' version='2' action='delete' timestamp='2021-11-28T05:14:24+00:00'>
+        assert len(m.bounds_list) == 0
+        assert len(m.node_dict) == 0
+        assert len(m.way_dict) == 0
+        assert len(m.relation_dict) == 0
+
 
 if __name__ == "__main__":
     unittest.main()
