8ce0c632abe1e02a12ab608368f44ae9bf663681
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2022-11-10 04:06:29 +0800

    Release 0.5.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Keqing_Sword",
-    version="0.5.1",
+    version="0.5.2",
     author="OSMChina",
     author_email="keaitianxinmail@qq.com",
     description="A non-database Python base OSM data parser, with SQL operation simulated",
