2afc47bde8b7d4ed942b1c59fb249c96a93e02cc
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2022-11-10 06:17:02 +0800

    feat(test_export.py): try to write a sax parser
    cross data correctness with different parser

--- tests/test_export.py
+++ tests/test_export.py
@@ -1,8 +1,10 @@
 # 尝试直接另存，完全新建对象另存，在已有基础增删改后另存
 
 import unittest
-import xml
+import xml.sax
+from xml import sax
 from os.path import dirname, join, realpath
+from xml.sax.handler import ContentHandler
 
 from kqs.waifu import Waifu
 
@@ -10,16 +12,29 @@ from kqs.waifu import Waifu
 class TestExport(unittest.TestCase):
     def setUp(self) -> None:
         self.map = Waifu()
-        FILENAME = "standard_load.osm"
-        data_path = join(dirname(realpath(__file__)), "export", FILENAME)
-        self.map.read(mode="file", file_path=data_path)
+        self.FILENAME_TARGET = "standard_load.osm"
+        self.FILENAME_OUTPUT = "self_save_dist.osm"
+        self.path_target = join(
+            dirname(realpath(__file__)), "export", self.FILENAME_TARGET
+        )
+        self.path_output = join(
+            dirname(realpath(__file__)), "export", self.FILENAME_OUTPUT
+        )
+        self.map.read(mode="file", file_path=self.path_target)
 
     def test_self_save(self):
         m = self.map
-        m.write(mode="file", file_path="self_save_dist.osm")
+        m.write(mode="file", file_path=self.path_output)
 
-        # src=xml.parsers
-        # dst=xml.parsers
+        class MapElementHandler(xml.sax.ContentHandler):
+            def __init__(self, file_name: str):
+                pass
+
+        sax.parse(
+            source=self.path_output,
+            handler=MapElementHandler(file_name=self.path_target),
+        )
+        # output=xml.parsers
 
     def test_insert_node_save(self):
         # 在标准读取的基础上插入点后保存
