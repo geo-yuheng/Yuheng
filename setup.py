97a7b04e33cf3c064a97d0ff1108dfb2e2db5ed6
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2022-11-11 10:14:07 +0800

    feat(setup.py): deprecated tricky setup.py hack
    Use outer batch script pre_build_process.py instead
    This method maybe suitable for more kind of config file when build

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
-import os
 import json
+import os
 
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    print("=="*10)
-    print(os.system("dir"))
-
-with open("global_const.json","r",encoding="utf-8") as kqs_const:
-    kqs_const_json=json.loads(kqs_const.read())
-    KQS_CORE_NAME=kqs_const_json["KQS_CORE_NAME"]
-    KQS_VERSION=kqs_const_json["KQS_VERSION"]
 
 setuptools.setup(
-    name=KQS_CORE_NAME,
-    version=KQS_VERSION,
+    name="__KQS_CORE_NAME__",
+    version="__KQS_VERSION__",
     author="OSMChina",
     author_email="keaitianxinmail@qq.com",
     description="A non-database Python base OSM data parser, with SQL operation simulated",
