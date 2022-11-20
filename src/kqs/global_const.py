b3b3f93d02f116047a396cdc3f3f22946b6912be
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2022-11-20 23:22:36 +0800

    fix: fix negligence in grammar

--- src/kqs/global_const.py
+++ src/kqs/global_const.py
@@ -1,6 +1,13 @@
 import json
+import os
 
-KQS_VERSION: str = json.load(open("global_const.json", "r"))["KQS_VERSION"]
-KQS_CORE_NAME: str = json.load(open("global_const.json", "r"))["KQS_CORE_NAME"]
+KQS_VERSION: str = json.load(
+    open(os.path.join(os.path.dirname(__file__), "global_const.json"), "r")
+)["KQS_VERSION"]
+KQS_CORE_NAME: str = json.load(
+    open(os.path.join(os.path.dirname(__file__), "global_const.json"), "r")
+)["KQS_CORE_NAME"]
 # 👴和刻晴小姐的结婚纪念日🥰
-KQS_START_ID: int = json.load(open("global_const.json", "r"))["KQS_START_ID"]
+KQS_START_ID: int = json.load(
+    open(os.path.join(os.path.dirname(__file__), "global_const.json"), "r")
+)["KQS_START_ID"]
--- src/kqs/type_constraint.py
+++ src/kqs/type_constraint.py
@@ -8,19 +8,22 @@ class Bounds:
         self.max_lat: float = float(attrib["maxlat"])
         self.max_lon: float = float(attrib["maxlon"])
         self.origin: str = attrib.get("origin", "")
-    
-    def align_serialization(self)->str:
-        serialize_format="SB_WB_NB_EB"
+
+    def align_serialization(self) -> str:
+        serialize_format = "SB_WB_NB_EB"
         # another choise is don't use B in output_format, and use A/B insteal of P/N for plus or minus sign
-        def num_serialization(degree:float):
-            if degree>=0:
-                return="P"+str(degree).replace(".","D")
+        def num_serialization(degree: float):
+            if degree >= 0:
+                return "P" + str(degree).replace(".", "D")
             else:
-                return=str(degree).replace("-","N").replace(".","D")
-        return serialize_format.replace("SB",num_serialization(min_lat))
-                            .replace("WB",num_serialization(min_lon))
-                            .replace("NB",num_serialization(max_lat))
-                            .replace("EB",num_serialization(max_lon))
+                return str(degree).replace("-", "N").replace(".", "D")
+
+        return (
+            serialize_format.replace("SB", num_serialization(min_lat))
+            .replace("WB", num_serialization(min_lon))
+            .replace("NB", num_serialization(max_lat))
+            .replace("EB", num_serialization(max_lon))
+        )
 
     def align_serialization(self) -> str:
         serialize_format = "SB_WB_NB_EB"
@@ -43,6 +46,7 @@ class Bounds:
     ):  # ->Tuple[float,float,float,float]:
         # return (min_lat,min_lon,max_lat,max_lon)
         pass
+
 
 class Member:
     def __init__(self, type: str, ref: int, role: str):
