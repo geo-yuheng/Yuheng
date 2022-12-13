18f062b85371264d4c411c286ea002077c40fe29
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2022-12-13 16:24:51 +0800

    fix(src/kqs/type_constraint.py): change `ref` to `id`
    id have more typical semantic in OSM data model

--- src/kqs/type_constraint.py
+++ src/kqs/type_constraint.py
@@ -49,17 +49,17 @@ class Bounds:
 
 
 class Member:
-    def __init__(self, type: str, ref: int, role: str):
+    def __init__(self, type: str, id: int, role: str):
         self.type: str = type
-        self.ref: int = ref
+        self.id: int = id
         self.role: str = role
         self.__type_backup: str = type
-        self.__ref_backup: int = ref
+        self.__id_backup: int = id
         self.__role_backup: str = role
 
     def has_diff(self) -> bool:
         return (
             self.type != self.__type_backup
-            or self.ref != self.__ref_backup
+            or self.id != self.__id_backup
             or self.role != self.__role_backup
         )
