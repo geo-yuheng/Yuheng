1461118cf07968d505699d99ee41fed2bc66329e
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-02-04 05:06:31 +0000

    Release 0.7.1
    From this release our publish changed to full poetry toolchain

--- pre_build_process.py
+++ pre_build_process.py
@@ -19,12 +19,12 @@ def main() -> None:
     setup_py_read = open("pyproject.toml", "r")
     setup_content = (
             setup_py_read.read()
-            .replace("__KQS_VERSION__", global_const["KQS_VERSION"])
-            .replace("__KQS_CORE_NAME__", global_const["KQS_CORE_NAME"])
+            .replace("__KEQING_CORE_NAME__", global_const["KEQING_CORE_NAME"])
+            .replace("__KEQING_VERSION__", global_const["KEQING_VERSION"])
         )
     setup_py_read.close()
-    os.remove("setup.py")
-    setup_py_write = open("setup.py", "w")
+    os.remove("pyproject.toml")
+    setup_py_write = open("pyproject.toml", "w")
     setup_py_write.write(setup_content)
     setup_py_write.close()
 
