d8e0733703c1e77662eea630f3f670b89103de77
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-02-08 16:25:44 +0800

    feat: nominatim transform

---
+++ src/keqing/method/nominatim.py
@@ -0,0 +1,11 @@
+def nominatim_request(keyword:str, type="relation"):
+    pass
+
+def nominatim_transform(id=None, keyword=None):
+    pass
+
+"""
+Usage:
+1. offer a one-click function to transform in a AreaSearch overpassql(allow both file IO and memory read)
+2. Only conduct a nominatim request and get ID
+"""
