a68c593ace5345f0f1c116d0c3b77f69aa30b4d6
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-02-08 16:31:34 +0800

    refactor: abstract a geocode layer and separate out the nominatim layer

--- src/keqing/method/nominatim.py
+++ src/keqing/method/nominatim.py
@@ -1,11 +1,6 @@
-def nominatim_request(keyword:str, type="relation"):
+def nominatim_request(keyword: str) -> list:
+    """
+    Usage:
+    Conduct a nominatim request and return result list
+    """
     pass
-
-def nominatim_transform(id=None, keyword=None):
-    pass
-
-"""
-Usage:
-1. offer a one-click function to transform in a AreaSearch overpassql(allow both file IO and memory read)
-2. Only conduct a nominatim request and get ID
-"""
---
+++ src/keqing/method/overpass.py
@@ -0,0 +1,14 @@
+def geocode_transform(id=None, keyword=None) -> int:
+    return 3600 * 1000000 + 1
+
+
+def geocode_replace():
+    """
+    Usage:
+    Offer a one-click function to transform in a AreaSearch overpassql(allow both file IO and memory read)
+    """
+
+    # 1. search and parse AreaSearch part in QL
+    # 2. import nominatim_request and get result list (def nominatim_request)
+    # 3. use first result and calc real id (def geocode_transform)
+    pass
