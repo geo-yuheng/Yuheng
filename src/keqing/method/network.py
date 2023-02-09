85ce1ff7c391e6e85d319431f0898334cf6da049
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-02-09 21:17:15 +0800

    feat(method\network.py): allow overpass list and more safe

--- src/keqing/method/network.py
+++ src/keqing/method/network.py
@@ -1,13 +1,61 @@
+from typing import Optional
+
 from keqing.basic.global_const import KEQING_CORE_NAME, KEQING_VERSION
 
 
-def get_server(server_name: str) -> str:
+def get_server(server_name: str) -> Optional[str]:
     server_list = {
-        "OSM": "https://www.openstreetmap.org/api/0.6/",
-        "OGF": "https://opengeofiction.net/api/0.6/",
-        "OHM": "https://www.openhistoricalmap.org/api/0.6",
+        "OSM": {"url": "https://www.openstreetmap.org/api/0.6/"},
+        "OGF": {"url": "https://opengeofiction.net/api/0.6/"},
+        "OHM": {"url": "https://www.openhistoricalmap.org/api/0.6"},
     }
-    return server_list[server_name]
+    return server_list.get(server_name)["url"]
+
+
+def get_overpass(overpass_name: str, server="") -> Optional[str]:
+    overpass_list = {
+        "osmde": {
+            "server": "OSM",
+            "url": "https://overpass-api.de/api/",
+            "region": "global",
+            "version": "unknown",
+        },
+        "kumi": {
+            "server": "OSM",
+            "url": "https://overpass.kumi.systems/api/",
+            "region": "global",
+            "version": "unknown",
+        },
+        "osmru": {
+            "server": "OSM",
+            "url": "http://overpass.openstreetmap.ru/cgi/",
+            "region": "global",
+            "version": "unknown",
+        },
+        "osmfr": {
+            "server": "OSM",
+            "url": "https//overpass.openstreetmap.fr/api/",
+            "region": "global",
+            "version": "unknown",
+        },
+        "ogf": {
+            "server": "OGF",
+            "url": "https//overpass.ogf.rent-a-planet.com/api/",
+            "region": "global",
+            "version": "unknown",
+        },
+    }
+
+    if server != "":
+        if (
+            overpass_list.get(overpass_name) != None
+            and overpass_list.get(overpass_name)["server"] != server
+        ):
+            return overpass_list.get(overpass_name)["url"]
+        else:
+            return None
+    else:
+        return overpass_list.get(overpass_name)["url"]
 
 
 def get_headers():
