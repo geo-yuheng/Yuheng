80860023a457a5f05facfb685398f18d474216d8
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-04-17 15:01:06 +0800

    feat(method_parse.py): new func prototype and testcase

--- src/kqs/method_parse.py
+++ src/kqs/method_parse.py
@@ -10,3 +10,20 @@ def t2type(t: str) -> str:
         return "changeset"
     if t == "v" or t == "version":
         return "version"
+
+def url_parse(url:str,domain:str="openstreetmap.org"):
+    type:str="node"
+    id:int=0
+#     import urlib
+    url=url.split(domain)
+    #test case
+    # node/way/relation/changeset
+    # openstreetmap.org/node/5739239358
+    #　http://www.openstreetmap.org/node/5739239358
+    # https://www.openstreetmap.org/node/5739239358#map=19/25.53658/113.53478
+    #　https://www.openstreetmap.org/node/5739239358#map=19/25.53658/113.53444&layers=TNDG
+    #　https://www.openstreetmap.org/node/5739239358?locale=zh-TW#map=19/25.53658/113.53444
+    # https://www.openstreetmap.org/node/5739239358/history#map=19/25.53668/113.53436
+    return [type,id]
+
+parse_url_fallback=["osm.org","openstreetmap.org", "openstreetmap.com"]# parse this js file https://github.com/openstreetmap/dns/blob/master/dnsconfig.js
