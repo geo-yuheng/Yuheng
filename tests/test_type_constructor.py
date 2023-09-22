12b7f5eca6f6506b5e3d01fe7ccedd033e01ea25
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2023-09-22 22:51:37 +0800

    chore: fake inlined data
    发现两个需要改进的地方
    1. 对于这种nwr互相包含互为成员的情况，在单纯进行一次网络请求的时候，可以提供一个“同时获取父子兄弟”的可选参数，以便于启发式获取上下级信息
    
    兄弟是route和route_master的另一个route的时候
    
    2. node或者way在声明的时候仍不够合理，考虑重载一个直接做参数的而非attrib字典做参数

--- tests/test_type_constructor.py
+++ tests/test_type_constructor.py
@@ -2,6 +2,7 @@
 # （以及允许人工new对象）
 
 import unittest
+
 # from os.path import dirname, join, realpath # 可能后期会导出存为.osm文件
 
 from src import keqing
@@ -13,8 +14,29 @@ class TestTypeConstructor(unittest.TestCase):
         self.map = keqing.Waifu()
 
     def test_construct_elements(self):
-        map=self.map
-        node_1=Node(attrib={"id":"114514","version":"1","changeset":"1"}, tag_dict={"building":"yes","colour":"red"})
+        map = self.map
+        node_1 = Node(
+            attrib={"id": "114514", 
+                    "visible":"true",
+                    "version": "1", 
+                    "changeset": "1"
+                    "timestamp":"2012-12-21T11:33:55Z",
+                    "user":"810",
+                    "uid":"1919",
+                    "lat":"39.5116939",
+                    "lon":"116.3682258"},
+            tag_dict={},
+        )
+        relation_1 = Relation(
+            attrib={"id": "114514", 
+                    "visible":"true",
+                    "version": "1", 
+                    "changeset": "1"
+                    "timestamp":"2012-12-21T11:33:55Z",
+                    "user":"810",
+                    "uid":"1919"},
+            tag_dict={"building": "yes", "colour": "red","building:prefabricated":"cai-gang-ban"},
+        )
 
         assert len(map.way_dict) == 1
 
