76e1fc1af6a39da733eccb02846002e3f30e0411
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2023-09-23 18:35:31 +0800

    chore: finish fake data and run black formatter
    可以考虑把这些外部假数据用pickle移动出去，但是那就会限制开发平台了，头疼，总不能以json送出去吧
    
    还是尽快考虑构造器重载吧

--- tests/test_type_constructor.py
+++ tests/test_type_constructor.py
@@ -16,103 +16,148 @@ class TestTypeConstructor(unittest.TestCase):
     def test_construct_elements(self):
         map = self.map
         node_1 = Node(
-            attrib={"id": "1", 
-                    "visible":"true",
-                    "version": "1", 
-                    "changeset": "1"
-                    "timestamp":"2012-12-21T11:33:55Z",
-                    "user":"810",
-                    "uid":"1919",
-                    "lat":"1.0",
-                    "lon":"5.0"},
+            attrib={
+                "id": "1",
+                "visible": "true",
+                "version": "1",
+                "changeset": "1",
+                "timestamp": "2012-12-21T11:33:55Z",
+                "user": "810",
+                "uid": "1919",
+                "lat": "1.0",
+                "lon": "5.0",
+            },
             tag_dict={},
         )
         node_2 = Node(
-            attrib={"id": "2", 
-                    "visible":"true",
-                    "version": "1", 
-                    "changeset": "1"
-                    "timestamp":"2012-12-21T11:33:55Z",
-                    "user":"810",
-                    "uid":"1919",
-                    "lat":"6.0",
-                    "lon":"5.0"},
+            attrib={
+                "id": "2",
+                "visible": "true",
+                "version": "1",
+                "changeset": "1",
+                "timestamp": "2012-12-21T11:33:55Z",
+                "user": "810",
+                "uid": "1919",
+                "lat": "6.0",
+                "lon": "5.0",
+            },
             tag_dict={},
         )
         node_3 = Node(
-            attrib={"id": "3", 
-                    "visible":"true",
-                    "version": "1", 
-                    "changeset": "1"
-                    "timestamp":"2012-12-21T11:33:55Z",
-                    "user":"810",
-                    "uid":"1919",
-                    "lat":"3.5",
-                    "lon":"1.0"},
+            attrib={
+                "id": "3",
+                "visible": "true",
+                "version": "1",
+                "changeset": "2",
+                "timestamp": "2012-12-22T11:33:55Z",
+                "user": "810",
+                "uid": "1919",
+                "lat": "3.5",
+                "lon": "1.0",
+            },
             tag_dict={},
         )
         node_4 = Node(
-            attrib={"id": "4", 
-                    "visible":"false",
-                    "version": "1", 
-                    "changeset": "1"
-                    "timestamp":"2012-12-21T11:33:55Z",
-                    "user":"810",
-                    "uid":"1919",
-                    "lat":"6.0",
-                    "lon":"5.0"},
+            attrib={
+                "id": "4",
+                "visible": "false",
+                "version": "1",
+                "changeset": "2",
+                "timestamp": "2012-12-22T11:33:55Z",
+                "user": "810",
+                "uid": "1919",
+                "lat": "6.0",
+                "lon": "5.0",
+            },
             tag_dict={},
         )
         node_5 = Node(
-            attrib={"id": "5", 
-                    "visible":"true",
-                    "version": "1", 
-                    "changeset": "1"
-                    "timestamp":"2012-12-21T11:33:55Z",
-                    "user":"810",
-                    "uid":"1919",
-                    "lat":"6.0",
-                    "lon":"5.0"},
+            attrib={
+                "id": "5",
+                "visible": "true",
+                "version": "1",
+                "changeset": "2",
+                "timestamp": "2012-12-22T11:33:55Z",
+                "user": "810",
+                "uid": "1919",
+                "lat": "6.0",
+                "lon": "5.0",
+            },
             tag_dict={},
         )
         node_6 = Node(
-            attrib={"id": "6", 
-                    "visible":"true",
-                    "version": "1", 
-                    "changeset": "1"
-                    "timestamp":"2012-12-21T11:33:55Z",
-                    "user":"810",
-                    "uid":"1919",
-                    "lat":"6.0",
-                    "lon":"5.0"},
+            attrib={
+                "id": "6",
+                "visible": "true",
+                "version": "1",
+                "changeset": "2",
+                "timestamp": "2012-12-22T11:33:55Z",
+                "user": "810",
+                "uid": "1919",
+                "lat": "6.0",
+                "lon": "5.0",
+            },
             tag_dict={},
         )
         node_7 = Node(
-            attrib={"id": "7", 
-                    "visible":"true",
-                    "version": "1", 
-                    "changeset": "1"
-                    "timestamp":"2012-12-21T11:33:55Z",
-                    "user":"810",
-                    "uid":"1919",
-                    "lat":"6.0",
-                    "lon":"5.0"},
+            attrib={
+                "id": "7",
+                "visible": "true",
+                "version": "1",
+                "changeset": "3",
+                "timestamp": "2012-12-23T11:33:55Z",
+                "user": "810",
+                "uid": "1919",
+                "lat": "6.0",
+                "lon": "5.0",
+            },
             tag_dict={},
         )
-        way_1=Way()
-        way_2=Way()
-        relation_1 = Relation(
-            attrib={"id": "114514", 
-                    "visible":"true",
-                    "version": "1", 
-                    "changeset": "1"
-                    "timestamp":"2012-12-21T11:33:55Z",
-                    "user":"810",
-                    "uid":"1919"},
-            tag_dict={"building": "yes", "colour": "red","building:prefabricated":"cai-gang-ban"},
+        way_1 = Way(
+            attrib={
+                "id": "114",
+                "visible": "true",
+                "version": "1",
+                "changeset": "3",
+                "timestamp": "2012-12-23T11:33:55Z",
+                "user": "810",
+                "uid": "1919",
+            },
+            tag_dict={},
+            nd_list=["1", "2", "3"],
         )
-
-        assert len(map.way_dict) == 1
+        way_2 = Way(
+            attrib={
+                "id": "514",
+                "visible": "true",
+                "version": "1",
+                "changeset": "3",
+                "timestamp": "2012-12-23T11:33:55Z",
+                "user": "810",
+                "uid": "1919",
+            },
+            tag_dict={},
+            nd_list=["5", "6", "7"],
+        )
+        relation_1 = Relation(
+            attrib={
+                "id": "114514",
+                "visible": "true",
+                "version": "1",
+                "changeset": "1",
+                "timestamp": "2012-12-21T11:33:55Z",
+                "user": "810",
+                "uid": "1919",
+            },
+            tag_dict={
+                "building": "yes",
+                "colour": "red",
+                "building:prefabricated": "cai-gang-ban",
+            },
+        )
+        assert len(map.node_dict) == 7
+        assert len(map.way_dict) == 2
+        assert len(map.relation_dict) == 1
 
 
 if __name__ == "__main__":
