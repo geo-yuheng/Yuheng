026ec2e0eb669d14eaa5dda5e1b55ba0161f6467
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-12-14 13:33:24 +0800

    feat: 尽管如此，如果用户指定需要某种模式

--- src/yuheng/plugin/driver_poly/__main__.py
+++ src/yuheng/plugin/driver_poly/__main__.py
@@ -1,8 +1,10 @@
 # import os
-from typing import Dict, List
+from typing import Dict, List, Tuple, Union
 
 
-def main(poly_file_path: str) -> List[Dict[str, float]]:
+def main(
+    poly_file_path: str, schema=None, order=None
+) -> List[Dict[str, float]]:
     # 暂时只处理单一环路的poly文件
 
     # 文件读取
@@ -12,10 +14,35 @@ def main(poly_file_path: str) -> List[Dict[str, float]]:
 
     # 文件处理
     poly_content = poly_content.split("END")[0].split("\n")[2:]
-    poly_object = [
-        dict(zip(["latitude", "longitude"], i[3:].split("   ")))
-        for i in list(filter(bool, poly_content))
-    ]
+
+    def line2node(
+        line_str: str,
+    ) -> Union[Dict[str, float], List[float], Tuple[float, float]]:
+        if schema == None:
+            return dict(
+                zip(
+                    ["longitude", "latitude"],  # ["经度", "纬度"]
+                    [float(f) for f in line_str.split("   ")],
+                )
+            )
+        elif isinstance(schema, type([])):
+            if order == "lon-lat":
+                return [float(f) for f in line_str.split("   ")]
+            else:  # lat-lon
+                return list(
+                    reversed([float(f) for f in line_str.split("   ")])
+                )
+        elif isinstance(schema, type(())):
+            if order == "lon-lat":
+                return tuple([float(f) for f in line_str.split("   ")])
+            else:  # lat-lon
+                return tuple(
+                    reversed([float(f) for f in line_str.split("   ")])
+                )
+        pass
+
+    poly_object = [line2node(i[3:]) for i in list(filter(bool, poly_content))]
+    print(poly_object)
     return poly_object
 
 
