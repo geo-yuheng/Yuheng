02ff5a9bda4d0b3e7430727a9a4e5a8a5e6b4602
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-01-10 21:36:47 +0800

    test: 简单记一下测试用例的想法

---
+++ tests/test_constraint.py
@@ -0,0 +1,16 @@
+from src.yuheng import Bounds
+
+Bounds(
+    {
+        "minlat": "11.4514",
+        "maxlat": "8",
+        "minlon": "-10",
+        "maxlon": "19.19",
+        "origin": "NONE SENSE",
+    }
+)
+
+# 输入
+# WW:-EE是(NN,SS)
+# 输入
+# WW,SS,EE,NN
