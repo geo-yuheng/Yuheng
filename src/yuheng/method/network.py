938d3ddf9dc8325c98de9c7e1f1dd08f4dccbd1d
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-01-10 18:56:50 +0800

    feat: 或者如果啥都不知道，那就返回OSM的。
    这最保险。
    
    也最不出错。
    虽然这是意味着我们有偏向性和不够中立的中心主义，但毕竟这就是大部分用户如果不特指的时候希望得到的结果
    
    此外，全小写容错

--- src/yuheng/method/network.py
+++ src/yuheng/method/network.py
@@ -8,67 +8,67 @@ from ..basic import YUHENG_CORE_NAME, YUHENG_VERSION
 # network 模块并不负责从网上读取数据，它负责的是endpoint和各种网络相关环境的处理。而从网络上读取数据是作为read driver的一种（因为并不仅仅有一种来源的driver）
 
 
-def get_endpoint_api(endpoint_name: str) -> Optional[str]:
+def get_endpoint_api(endpoint_name="osm") -> Optional[str]:
     endpoint_api_list = {
-        "OSM": {"url": "https://api.openstreetmap.org/api", "version": 0.6},
-        "OGF": {"url": "https://opengeofiction.net/api", "version": 0.6},
-        "OHM": {
+        "osm": {"url": "https://api.openstreetmap.org/api", "version": 0.6},
+        "ogf": {"url": "https://opengeofiction.net/api", "version": 0.6},
+        "ohm": {
             "url": "https://www.openhistoricalmap.org/api",
             "version": 0.6,
         },
-        "OSM-api06": {
+        "osm-api06": {
             "url": "https://api06.dev.openstreetmap.org/api",
             "version": 0.6,
         },
-        "OSM-dev": {
+        "osm-dev": {
             "url": "https://master.apis.dev.openstreetmap.org/api",
             "version": 0.6,
         },
     }
-    return endpoint_api_list.get(endpoint_name)["url"]
+    return endpoint_api_list.get(endpoint_name.lower())["url"]
 
 
-def get_endpoint_overpass(endpoint_name: str, server="") -> Optional[str]:
+def get_endpoint_overpass(endpoint_name="osmde") -> Optional[str]:
     endpoint_overpass_list = {
         "osmde": {
-            "server": "OSM",
+            "server": "osm",
             "url": "https://overpass-api.de/api/",
             "region": "global",
             "version": "unknown",
         },
         "kumi": {
-            "server": "OSM",
+            "server": "osm",
             "url": "https://overpass.kumi.systems/api/",
             "region": "global",
             "version": "unknown",
         },
         "osmru": {
-            "server": "OSM",
+            "server": "osm",
             "url": "http://overpass.openstreetmap.ru/cgi/",
             "region": "global",
             "version": "unknown",
         },
         "osmfr": {
-            "server": "OSM",
+            "server": "osm",
             "url": "https://overpass.openstreetmap.fr/api/",
             "region": "global",
             "version": "unknown",
         },
         "ogf": {
-            "server": "OGF",
+            "server": "ogf",
             "url": "https//overpass.ogf.rent-a-planet.com/api/",
             "region": "global",
             "version": "unknown",
         },
         "ohm": {
-            "server": "OHM",
+            "server": "ohm",
             "url": "https://overpass-api.openhistoricalmap.org/api/",
             "region": "global",
             "version": "unknown",
         },
     }
 
-    return endpoint_overpass_list.get(endpoint_name)["url"]
+    return endpoint_overpass_list.get(endpoint_name.lower())["url"]
 
 
 def get_headers() -> dict:
