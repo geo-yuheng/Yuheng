d35c18af2e11ae275b862a8c04238fc781463620
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-01-11 14:24:46 +0800

    feat: generate correct metadata part

--- src/yuheng/plugin/overpass/overpass_generate.py
+++ src/yuheng/plugin/overpass/overpass_generate.py
@@ -1,4 +1,27 @@
+from typing import Optional, Union
+
 query_template = "<metadata_part>\n<condition_part>\n<output_part>"
+
+
+def gen_metadata(**kwargs) -> Optional[str]:
+    buffer = ""
+
+    def wrap_metadata(key: str, value: Union[str, int, float]) -> str:
+        if isinstance(value, int) or isinstance(value, float):
+            value = str(value)
+        return f"[{key}:{value}]"
+
+    if kwargs.get("metadata_entry_data"):
+        for metadata_entry in kwargs.get("metadata_entry_data"):
+            # print(kwargs.get("metadata_entry_data").get(metadata_entry))
+            buffer += wrap_metadata(
+                metadata_entry,
+                kwargs.get("metadata_entry_data").get(metadata_entry, ""),
+            )
+    if buffer != "":
+        return buffer + ";"
+    else:
+        return None
 
 
 def gen_constraint(constraint_type: str) -> str:
@@ -21,3 +44,9 @@ def gen_query():
         .replace("<condition_part>", gen_condition())
         .replace("<output_part>", gen_output())
     )
+
+
+testcase_gen_metadata = gen_metadata(
+    metadata_entry_data={"out": "xml", "timeout": 255}
+)
+print(testcase_gen_metadata)
