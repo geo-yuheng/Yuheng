6da54d710518c8d7ed58b647f8d15325f920a817
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-01-24 22:44:26 +0800

    feat: 确定读写是多格式兼容的

--- src/yuheng/plugin/driver_geojson/__main__.py
+++ src/yuheng/plugin/driver_geojson/__main__.py
@@ -1,21 +1,51 @@
 import os
+import sys
+
 import geojson
 
-def read(json_file_path: str, output_target=None)->geojson.feature.FeatureCollection:
+current_dir = os.path.dirname(os.path.realpath(__file__))
+src_dir = os.path.join(current_dir, "..", "..", "..")
+sys.path.append(src_dir)
+from yuheng import Waifu
+
+
+def read(
+    json_file_path: str, output_target=None
+) -> Union[dict, str, geojson.feature.FeatureCollection, Waifu]:
     """
-    ourput_target: 输出其他某种文本格式？直出库读出来的geojson对象？dict？某种特定的字符串序列化？Waifu？
+    ourput_target:
+    * yuheng: 输出Waifu对象
+    * dict: 输出Python原生的dict对象
+    * geojson: 输出geojson.feature.FeatureCollection对象
+    * serialize: 输出特定格式的序列化字符串对象
     """
-    # 文件读取
     json_file = open(json_file_path, "r", encoding="utf-8")
     json_content = json_file.read()
     json_file.close()
-    geojson_obj=geojson.loads(json_content)
+    geojson_obj = geojson.loads(json_content)
 
     from pprint import pprint
+
     pprint(geojson_obj)
     return geojson_obj
 
-def write():
+
+def write(
+    yuheng_obj: Waifu, output_target=None, **kwargs
+) -> Union[dict, str, geojson.feature.FeatureCollection]:
+    """
+    ourput_target:
+    * dict: 输出Python原生的dict对象
+    * str: 输出将geojson序列化后的字符串对象
+    * geojson: 输出geojson.feature.FeatureCollection对象
+    flag:
+    读到的node使用若干个Point还是MultiPoint
+    """
+    from geojson import Point
+
+    for id in Waifu.way_dict:
+        single_node = Waifu.way_dict[id]
+        geojson_point_ojb = Point(single_node.lon, single_node.lat)
     pass
 
 
