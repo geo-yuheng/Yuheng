b9652cca0067a4291674c2c8ebce285475af9861
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2024-02-28 15:01:59 +0800

    fix: changed folder structure haven't been impled

--- tests/cases/plugin_driver_poly.py
+++ tests/cases/plugin_driver_poly.py
@@ -4,6 +4,7 @@ import os
 import sys
 import unittest
 
+
 current_dir = os.path.dirname(os.path.realpath(__file__))
 src_dir = os.path.join(current_dir, "../../src")
 sys.path.append(src_dir)
@@ -19,6 +20,8 @@ class TestPluginDriverPoly(unittest.TestCase):
         ans = main(
             poly_file_path=os.path.join(
                 os.getcwd(),
+                "..",
+                "assets",
                 "poly",
                 "Izaland-polyfile-20231213-laoshubabytest.poly",
             )
@@ -30,6 +33,8 @@ class TestPluginDriverPoly(unittest.TestCase):
             "--poly_file_path "
             + os.path.join(
                 os.getcwd(),
+                "..",
+                "assets",
                 "poly",
                 "Izaland-polyfile-20231213-laoshubabytest.poly",
             )
@@ -40,6 +45,7 @@ class TestPluginDriverPoly(unittest.TestCase):
             + " "
             + os.path.join(
                 os.getcwd(),
+                "..",
                 "..",
                 "src",
                 "yuheng",
