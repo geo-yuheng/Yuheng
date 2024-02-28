550e265468e381b11679959ec87bc39040ce0ba2
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2024-02-29 01:50:14 +0800

    refactor: folium is continue add object so more suitable as class

--- src/yuheng/plugin/folium/__main__.py
+++ src/yuheng/plugin/folium/__main__.py
@@ -10,10 +10,33 @@ sys.path.append(src_dir)
 from yuheng import Waifu, Way, Node, Relation
 
 
-def display(element: Union[Waifu, Way, Node, Relation]) -> None:
-    if isinstance(element, type(Way({"id": "114"}, {}, []))):
-        print("This is a Way")
-    return None
+class Map:
+    def __init__(self):
+        self.element_list = []
+
+    def add(self, element: Union[Waifu, Way, Node, Relation]) -> None:
+        """
+        Append some element to list
+        """
+        self.element_list.append(element)
+        return None
+
+    def display(self) -> None:
+        """
+        This func display some element to html
+        """
+
+        m = folium.Map(location=[11.4, 51.4], zoom_start=0)
+
+        for element in self.element_list:
+            if isinstance(element, type(Way({"id": "114"}, {}, []))):
+                print("This is a Way")
+                folium.PolyLine([(11.4, 51.4), (11.4, 51.4)]).add_to(m)
+                folium.Marker([11.4, 51.4]).add_to(m)
+
+        # gen html file or call webbrowser
+        m.save("index.html")
+        return None
 
 
 def main() -> 1:
