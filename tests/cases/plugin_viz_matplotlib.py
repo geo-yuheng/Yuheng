49019c714a515ede5aba7cedd1b2d7d4b0963d69
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2024-02-29 15:26:55 +0800

    feat: draw a fig (WIP)

---
+++ tests/cases/plugin_viz_matplotlib.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import unittest
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+src_dir = os.path.join(current_dir, "../../src")
+sys.path.append(src_dir)
+
+from yuheng import Carto
+from yuheng.type import Member, Node, Relation, Way
+from yuheng.plugin.viz_matplotlib.__main__ import init
+
+
+class TestPluginVisualizationMatplotlib(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_plugin_viz_folium_display_object(self):
+        init(1600, 900, 12.5, -3, -2.7, 6.66)
+
+
+if __name__ == "__main__":
+    unittest.main()
