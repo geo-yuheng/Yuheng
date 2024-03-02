59144c3a7c1227973a06cfba598fd07cb9578e29
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2024-03-02 22:14:43 +0800

    test: replace hardcoded value with variables

--- tests/cases/plugin_driver_db_postgresql.py
+++ tests/cases/plugin_driver_db_postgresql.py
@@ -2,17 +2,29 @@ import os
 import sys
 import unittest
 
+
 current_dir = os.path.dirname(os.path.realpath(__file__))
 src_dir = os.path.join(current_dir, "../../src")
 sys.path.append(src_dir)
 
 from yuheng import Carto
+from yuheng.basic import get_yuheng_path
 
 
 class TestPluginDriverDbPostgresql(unittest.TestCase):
     def setUp(self) -> None:
         # NOTE: conduct this part of postgresql need you deploy a server.
-        pass
+        self.database_profile = json.load(
+            open(
+                os.path.join(
+                    get_yuheng_path(),
+                    "db_profiles",
+                    "postgresql.db_profiles.yuheng",
+                ),
+                "r",
+                encoding="utf-8",
+            )
+        )
 
     def test_plugin_driver_db_postgresql_full_type_node(self):
         import time
@@ -21,11 +33,11 @@ class TestPluginDriverDbPostgresql(unittest.TestCase):
 
         start_time = time.time()
         world = get_data(
-            connection_dbname="osm2pgsql",
-            connection_user="postgres",
-            connection_password="12345678",
-            connection_host="localhost",
-            connection_port="5432",
+            connection_dbname=database_profile["dbname"],
+            connection_user=database_profile["dbname"],
+            connection_password=database_profile["password"],
+            connection_host=database_profile["host"],
+            connection_port=database_profile["port"],
             query_mode="full",
             query_type=["point"],
         )
@@ -43,11 +55,11 @@ class TestPluginDriverDbPostgresql(unittest.TestCase):
 
         start_time = time.time()
         world = get_data(
-            connection_dbname="osm2pgsql",
-            connection_user="postgres",
-            connection_password="12345678",
-            connection_host="localhost",
-            connection_port="5432",
+            connection_dbname=database_profile["dbname"],
+            connection_user=database_profile["user"],
+            connection_password=database_profile["password"],
+            connection_host=database_profile["host"],
+            connection_port=database_profile["port"],
             query_mode="full",
             query_type=["line"],
         )
@@ -65,11 +77,11 @@ class TestPluginDriverDbPostgresql(unittest.TestCase):
 
         start_time = time.time()
         carto = get_data(
-            connection_dbname="osm2pgsql",
-            connection_user="postgres",
-            connection_password="12345678",
-            connection_host="localhost",
-            connection_port="5432",
+            connection_dbname=database_profile["dbname"],
+            connection_user=database_profile["user"],
+            connection_password=database_profile["password"],
+            connection_host=database_profile["host"],
+            connection_port=database_profile["port"],
             query_mode="full",
             query_type=["line", "point"],
         )
@@ -83,11 +95,11 @@ class TestPluginDriverDbPostgresql(unittest.TestCase):
         from yuheng.plugin.driver_db_postgresql.__main__ import get_data
 
         result = get_data(
-            connection_dbname="osm2pgsql",
-            connection_user="postgres",
-            connection_password="12345678",
-            connection_host="localhost",
-            connection_port="5432",
+            connection_dbname=database_profile["dbname"],
+            connection_user=database_profile["user"],
+            connection_password=database_profile["password"],
+            connection_host=database_profile["host"],
+            connection_port=database_profile["port"],
             query_mode="full",
             query_type=["line", "point", "polygon"],
         )
