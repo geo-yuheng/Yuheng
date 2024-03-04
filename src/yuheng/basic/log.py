4c5a74abfd323af7debd69ebb0790f7ee41b26f1
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2024-03-05 02:52:05 +0800

    feat: success run aliyun sls handler
    1. 傻逼logging，不全局声明不行，不然只能引入工厂模式来创建获得不同conf的对象。一个conf管全局，真是罕见。
    2. 傻逼logging，不直接暴露handler，只能从logger中遍历获取handler。
    3. 傻逼logging，conf写的和山一样长，不如loguru。即使没有loguru，也会有亿万个logugugu，logrururu站出来打倒logging
    4. 傻逼阿里云，示例代码一塌糊涂，文档要分别在 https://github.com/aliyun/aliyun-log-python-sdk/blob/master/README_CN.md | https://aliyun-log-python-sdk.readthedocs.io/tutorials/tutorial_logging_handler.html | https://aliyun-log-cli.readthedocs.io/en/latest/tutorials/tutorial_log_upload.html# 好几个地方分别看，而且似乎一个项目既是云资源管理器，又是程序内可以import的包，形态宛如共轭父子。
    5. 傻逼阿里云，没有logtail这个自己造的agent就传不上日志？必须有自己的ECS？要写日志，你需要先创建project，再在project里面创建logstore，再在logstore里面创建logtails和机器组，一个logtails可能涉及若干组机器。然后还可能要创建shard。然后你才能用logtails把日志（还得类型对了，单行多行json文件得选）传去服务。
    
    NB，这一天就光搞日志了
    
    我搞个S3的API把本地文件落盘了打上去都好
    
    openobserve是目前唯一见过在Windows有客户端的，其他包括elasticsearch就太重，不太重的loki或者oklog或者tinylog或者LogDevice或者clickhouse都没有windows客户端。然后都不维护了。seq没停止维护但是不是开源的。选了半天就只能clickhouse或者openobverse
    
    三天啊，全都用来找log方案了！除了本地方案以外别的都不行。堪比当时选数据库的时候cratedb不行，metaease上直接搭建/docker搭建都联不通。各自配置文件改了没用。zeabur的镜像能通但是太贵。其实还应该试试edb不过没意思了。

--- src/yuheng/basic/log.py
+++ src/yuheng/basic/log.py
@@ -1,16 +1,57 @@
 import os
+import logging.config
 
-from loguru import logger
+from loguru import logger as loguru_logger
 
 from .global_const import get_yuheng_path
 
-logger_config = {"level": "INFO"}
-logger_format = "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | {name}:{function}:{line} - {message}"
+logging_config = {
+    "version": 1,
+    "formatters": {
+        "rawformatter": {"class": "logging.Formatter", "format": "%(message)s"}
+    },
+    "handlers": {
+        "sls_handler": {
+            "()": "aliyun.log.QueuedLogHandler",
+            "level": "INFO",
+            "formatter": "rawformatter",
+            "end_point": "cn-qingdao.log.aliyuncs.com",
+            "access_key_id": "",
+            "access_key": "",
+            "project": "yuhenglog",
+            "log_store": "loggggggggggg",
+        }
+    },
+    "loggers": {
+        "sls": {
+            "handlers": [
+                "sls_handler",
+            ],
+            "level": "INFO",
+            "propagate": False,
+        }
+    },
+}
+logging.config.dictConfig(logging_config)
+logging_handler_sls = logging.getLogger("sls").handlers[0]
 
+loguru_config = {"level": "INFO"}
+loguru_format = "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | {name}:{function}:{line} - {message}"
+logger = loguru_logger
+
+
+# file
 logger.add(
     sink=os.path.join(get_yuheng_path(), "log", "log_{time}.log"),
-    format=logger_format,
-    **logger_config
+    format=loguru_format,
+    **loguru_config
 )
+# aliyun sls
+logger.add(sink=logging_handler_sls, format=loguru_format, **loguru_config)
+# clickhouse
+# logger.add(sink=logging_handler_clickhouse)
+# openobserve
+# logger.add(sink=logging_handler_openobserve)
+
 # logger.disable("yuheng")
 logger.info("loguru enabled")
