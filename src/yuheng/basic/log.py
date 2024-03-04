107ccedc9bb6aed490025694307aac8948b53970
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2024-03-05 03:06:33 +0800

    fix: disable unstable aliyun sls sdk
    我以为是我看不懂文档，头疼的怀疑人生
    
    后来发现logging的设计可能是和java学的，一点也不pythonic，而且故意不暴露和希望你引入太多的类。但是耐心可能未来能看懂只是很费力和时间
    
    阿里云的文档吧，可能确实就不是让我看懂的。其实github README的例子跟着做做还能很快上手，但是他的业务设计呢，就是那一层层的结构
    我觉得吧，即使是企业级也不一定要用到这么多层次的管理的，可能是给阿里内部用来刷管理粒度的KPI的时候才会有用
    
    建议明天电话问问是不是不能在非阿里云机器上不用logtails直接打，或者能在非阿里云机器打log但是要安那agent，通过agent打，不能http
    
    离谱，client能http操作加logstore不能http打log？
    
    如果真的限制这么严不如没有，这时间不如给clickhouse了，clickhouse还有一个python的库呢，也是logging handler
    https://github.com/hashbash/clickhouse-logging
    虽然不是loguru的，但是毕竟loguru是新人
    
    openobserve似乎还是从fluentd/filebeat/promethus.grafana等来源接入采集的，或者文件导入，似乎没有python直接打它5432或者3306端口就能写的，要起一个web服务
    
    https://openobserve.ai/docs/ingestion/traces/python/#setup-serviceapplication
    
    麻
    
    不如还是看看远处的AWS吧，看看AWS有没有提供一些简单的log api，访问即打。

--- src/yuheng/basic/log.py
+++ src/yuheng/basic/log.py
@@ -1,39 +1,39 @@
 import os
-import logging.config
+# import logging.config
 
 from loguru import logger as loguru_logger
 
 from .global_const import get_yuheng_path
 
-logging_config = {
-    "version": 1,
-    "formatters": {
-        "rawformatter": {"class": "logging.Formatter", "format": "%(message)s"}
-    },
-    "handlers": {
-        "sls_handler": {
-            "()": "aliyun.log.QueuedLogHandler",
-            "level": "INFO",
-            "formatter": "rawformatter",
-            "end_point": "cn-qingdao.log.aliyuncs.com",
-            "access_key_id": "",
-            "access_key": "",
-            "project": "yuhenglog",
-            "log_store": "loggggggggggg",
-        }
-    },
-    "loggers": {
-        "sls": {
-            "handlers": [
-                "sls_handler",
-            ],
-            "level": "INFO",
-            "propagate": False,
-        }
-    },
-}
-logging.config.dictConfig(logging_config)
-logging_handler_sls = logging.getLogger("sls").handlers[0]
+# logging_config = {
+#     "version": 1,
+#     "formatters": {
+#         "rawformatter": {"class": "logging.Formatter", "format": "%(message)s"}
+#     },
+#     "handlers": {
+#         "sls_handler": {
+#             "()": "aliyun.log.QueuedLogHandler",
+#             "level": "INFO",
+#             "formatter": "rawformatter",
+#             "end_point": "cn-qingdao.log.aliyuncs.com",
+#             "access_key_id": "",
+#             "access_key": "",
+#             "project": "yuhenglog",
+#             "log_store": "loggggggggggg",
+#         }
+#     },
+#     "loggers": {
+#         "sls": {
+#             "handlers": [
+#                 "sls_handler",
+#             ],
+#             "level": "INFO",
+#             "propagate": False,
+#         }
+#     },
+# }
+# logging.config.dictConfig(logging_config)
+# logging_handler_sls = logging.getLogger("sls").handlers[0]
 
 loguru_config = {"level": "INFO"}
 loguru_format = "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | {name}:{function}:{line} - {message}"
@@ -47,7 +47,7 @@ logger.add(
     **loguru_config
 )
 # aliyun sls
-logger.add(sink=logging_handler_sls, format=loguru_format, **loguru_config)
+# logger.add(sink=logging_handler_sls, format=loguru_format, **loguru_config)
 # clickhouse
 # logger.add(sink=logging_handler_clickhouse)
 # openobserve
