fc2d156e16967808e5f82f2a022eea337ce131d1
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2024-03-06 03:32:55 +0800

    refactor: put those 2 part of code in func

--- src/yuheng/plugin/stat_metadata_clustering/__main__.py
+++ src/yuheng/plugin/stat_metadata_clustering/__main__.py
@@ -3,6 +3,7 @@ import os
 import sys
 import datetime
 import zoneinfo
+from typing import Tuple, Dict
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 src_dir = os.path.join(current_dir, "..", "..", "..")
@@ -40,62 +41,73 @@ def main(**kwargs):
             logger.info(f'Unrecognizable value from key "{k}"')
             pass
 
-        # reverse
-        metadata_frame_raw = []
-        for id, element in world.node_dict.items():
-            metadata_frame_raw.append(
-                (
-                    "n" + str(element.id),
-                    element.timestamp,
-                    element.uid,
-                    element.changeset,
+        def get_metadata_frame_raw(world: Carto) -> Tuple[str, int, int, int]:
+            metadata_frame_raw = []
+            for id, element in world.node_dict.items():
+                metadata_frame_raw.append(
+                    (
+                        "n" + str(element.id),
+                        element.timestamp,
+                        element.uid,
+                        element.changeset,
+                    )
                 )
-            )
-        for id, element in world.way_dict.items():
-            metadata_frame_raw.append(
-                (
-                    "w" + str(element.id),
-                    element.timestamp,
-                    element.uid,
-                    element.changeset,
+            for id, element in world.way_dict.items():
+                metadata_frame_raw.append(
+                    (
+                        "w" + str(element.id),
+                        element.timestamp,
+                        element.uid,
+                        element.changeset,
+                    )
                 )
-            )
-        for id, element in world.relation_dict.items():
-            metadata_frame_raw.append(
-                (
-                    "r" + str(element.id),
-                    element.timestamp,
-                    element.uid,
-                    element.changeset,
+            for id, element in world.relation_dict.items():
+                metadata_frame_raw.append(
+                    (
+                        "r" + str(element.id),
+                        element.timestamp,
+                        element.uid,
+                        element.changeset,
+                    )
                 )
-            )
-        logger.debug(metadata_frame_raw[0])
-        logger.debug(len(metadata_frame_raw))
-        metadata_frame = []  # 因为暂时不引入pandas所以用字典做行，用了pandas就直接行数组插入了。
-        for item in metadata_frame_raw:
-            timezone_server = datetime.timezone.utc
-            timezone_user = zoneinfo.ZoneInfo(LOCAL_TIMEZONE)
-            time_utc_str = item[1]
-            time_utc = datetime.datetime.strptime(
-                time_utc_str, "%Y-%m-%dT%H:%M:%SZ"
-            )
-            time_utc = time_utc.replace(tzinfo=timezone_server)
-            time_user = time_utc.astimezone(timezone_user)
+            return metadata_frame_raw
 
-            metadata_frame.append(
-                {
-                    "id": item[0],
-                    "uid": item[2],
-                    "changeset": item[3],
-                    "time_year": time_user.year,
-                    "time_month": time_user.month,
-                    "time_day": time_user.day,
-                    "time_hour": time_user.hour,
-                    "time_minute": time_user.minute,
-                    "time_second": time_user.second,
-                }
-            )
-        logger.debug(metadata_frame[0])
+        def get_metadata_frame(
+            metadata_frame_raw: Tuple[str, int, int, int]
+        ) -> Dict[str, str]:
+            metadata_frame = []  # 因为暂时不引入pandas所以用字典做行，用了pandas就直接行数组插入了。
+            for item in metadata_frame_raw:
+                timezone_server = datetime.timezone.utc
+                timezone_user = zoneinfo.ZoneInfo(LOCAL_TIMEZONE)
+                time_utc_str = item[1]
+                time_utc = datetime.datetime.strptime(
+                    time_utc_str, "%Y-%m-%dT%H:%M:%SZ"
+                )
+                time_utc = time_utc.replace(tzinfo=timezone_server)
+                time_user = time_utc.astimezone(timezone_user)
+
+                metadata_frame.append(
+                    {
+                        "id": item[0],
+                        "uid": str(item[2]),
+                        "changeset": str(item[3]),
+                        "time_year": time_user.year,
+                        "time_month": time_user.month,
+                        "time_day": time_user.day,
+                        "time_hour": time_user.hour,
+                        "time_minute": time_user.minute,
+                        "time_second": time_user.second,
+                    }
+                )
+            return metadata_frame
+
+        metadata_frame_raw = get_metadata_frame_raw(world)
+        metadata_frame = get_metadata_frame(metadata_frame_raw)
+        logger.debug(metadata_frame[3000])
+        logger.debug(len(metadata_frame))
+
+        for 
+        clustering_uid=
 
 
 if __name__ == "__main__":
