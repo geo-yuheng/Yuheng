0a8392bfcd4ef0d5a71cfac75de1061fba3cb75c
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2024-03-05 17:55:14 +0800

    feat: successful on print cloud log
    日志服务能打了，但是什么都没做，只是去阿里云上请务必在logstore界面点一下“数据接入”，然后选择Python。虽然什么都不需要配置只要直接下一步就行，但是不做这一步它的数据池子就不会收你的数据。
    
    类似开通服务，但是他们的文档从来都只写开通服务，没有写这个“接入数据”才是实质性的开通服务。一般人就真的以为只是开通一个SLS就可以了。
    
    ……
    
    另外，各位调试的时候，添加了环境变量最好在系统变量和用户变量都添加一次。然后添加了以后我们都知道需要重启cmd不然获取不到。对于pycharm/vscode，也是要重启软件才行的。你在它里面去重新开一个terminal是不能重新获取environ的。

--- src/yuheng/basic/log.py
+++ src/yuheng/basic/log.py
@@ -1,58 +1,59 @@
 import os
 
-# import logging.config
+import logging.config
 
 from loguru import logger as loguru_logger
 
 from .global_const import get_yuheng_path
 
-# logging_config = {
-#     "version": 1,
-#     "formatters": {
-#         "rawformatter": {"class": "logging.Formatter", "format": "%(message)s"}
-#     },
-#     "handlers": {
-#         "sls_handler": {
-#             "()": "aliyun.log.QueuedLogHandler",
-#             "level": "INFO",
-#             "formatter": "rawformatter",
-#             "end_point": "cn-qingdao.log.aliyuncs.com",
-#             "access_key_id": "",
-#             "access_key": "",
-#             "project": "yuhenglog",
-#             "log_store": "loggggggggggg",
-#         }
-#     },
-#     "loggers": {
-#         "sls": {
-#             "handlers": [
-#                 "sls_handler",
-#             ],
-#             "level": "INFO",
-#             "propagate": False,
-#         }
-#     },
-# }
-# logging.config.dictConfig(logging_config)
-# logging_handler_sls = logging.getLogger("sls").handlers[0]
+logging_config = {
+    "version": 1,
+    "formatters": {
+        "rawformatter": {"class": "logging.Formatter", "format": "%(message)s"}
+    },
+    "handlers": {
+        "sls_handler": {
+            "()": "aliyun.log.QueuedLogHandler",
+            "level": "INFO",
+            "formatter": "rawformatter",
+            "end_point": "cn-qingdao.log.aliyuncs.com",
+            "access_key_id": os.environ.get("ALIYUN_ACCESSKEY_ID"),
+            "access_key": os.environ.get("ALIYUN_ACCESSKEY_SECRET"),
+            "project": "yuhenglog",
+            "log_store": "dev",
+        }
+    },
+    "loggers": {
+        "sls": {
+            "handlers": [
+                "sls_handler",
+            ],
+            "level": "INFO",
+            "propagate": False,
+        }
+    },
+}
+logging.config.dictConfig(logging_config)
+logging_handler_aliyun = logging.getLogger("sls").handlers[0]
 
 loguru_config = {"level": "INFO"}
 loguru_format = "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | {name}:{function}:{line} - {message}"
 logger = loguru_logger
 
-
-# file
+# local-file
 logger.add(
     sink=os.path.join(get_yuheng_path(), "log", "log_{time}.log"),
     format=loguru_format,
     **loguru_config
 )
-# aliyun sls
-# logger.add(sink=logging_handler_sls, format=loguru_format, **loguru_config)
+# saas_aliyun_sls
+logger.add(sink=logging_handler_aliyun, format=loguru_format, **loguru_config)
+# saas_aws_cloudwatch
+# logger.add(sink=logging_handler_aws) # WIP
 # clickhouse
-# logger.add(sink=logging_handler_clickhouse)
+# logger.add(sink=logging_handler_clickhouse) # WIP
 # elasticsearch
-# logger.add(sink=logging_handler_elasticsearch)
+# logger.add(sink=logging_handler_elasticsearch) # WIP
 
 # logger.disable("yuheng")
 logger.info("loguru enabled")
