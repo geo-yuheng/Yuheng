872fa6af78930a10ac2c34837d7b2f6a7103d7c3
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2024-03-09 17:30:35 +0800

    performance: try to use numba jit
    Maybe this the only place that do pure for loop calculation so that it can be optimize by numba

--- src/yuheng/__init__.py
+++ src/yuheng/__init__.py
@@ -44,7 +44,11 @@ class Carto:
         if value is not None:
             attrib[key] = str(value)
 
-    @staticmethod
+    # @staticmethod
+
+    import numba
+
+    @numba.jit(force_obj=False, loof_left=False, nopython=False)
     def insert_to_dict(spec_dict, element_list):
         for i in element_list:
             spec_dict[int(i.id)] = i
