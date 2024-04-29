a186e409823abdd227a2fae1fb35b5d8f71a6deb
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-04-29 21:20:56 +0800

    fix: better practice in organize package function

--- tests/cases/plugin/plugin_driver_db_postgresql.py
+++ tests/cases/plugin/plugin_driver_db_postgresql.py
@@ -9,6 +9,7 @@ sys.path.append(src_dir)
 
 from yuheng import Carto
 from yuheng.basic import get_yuheng_path, logger
+from yuheng_plugin.yuheng_driver_db_postgresql import database_profile
 
 
 class TestPluginDriverDbPostgresql(unittest.TestCase):
@@ -29,7 +30,7 @@ class TestPluginDriverDbPostgresql(unittest.TestCase):
     def test_plugin_driver_db_postgresql_full_type_node(self):
         import time
 
-        from yuheng_plugin.yuheng_driver_db_postgresql.__main__ import get_data
+        from yuheng_plugin.yuheng_driver_db_postgresql import get_data
 
         start_time = time.time()
         world = get_data(
@@ -51,7 +52,7 @@ class TestPluginDriverDbPostgresql(unittest.TestCase):
     def test_plugin_driver_db_postgresql_full_type_way(self):
         import time
 
-        from yuheng_plugin.yuheng_driver_db_postgresql.__main__ import get_data
+        from yuheng_plugin.yuheng_driver_db_postgresql import get_data
 
         start_time = time.time()
         world = get_data(
@@ -73,7 +74,7 @@ class TestPluginDriverDbPostgresql(unittest.TestCase):
     def test_plugin_driver_db_postgresql_full_type_multi(self):
         import time
 
-        from yuheng_plugin.yuheng_driver_db_postgresql.__main__ import get_data
+        from yuheng_plugin.yuheng_driver_db_postgresql import get_data
 
         start_time = time.time()
         carto = get_data(
@@ -92,7 +93,7 @@ class TestPluginDriverDbPostgresql(unittest.TestCase):
         logger.info(f"total time: {duration}s")
 
     def test_plugin_driver_db_postgresql_full_invalidtype(self):
-        from yuheng_plugin.yuheng_driver_db_postgresql.__main__ import get_data
+        from yuheng_plugin.yuheng_driver_db_postgresql import get_data
 
         result = get_data(
             connection_dbname=database_profile["dbname"],
